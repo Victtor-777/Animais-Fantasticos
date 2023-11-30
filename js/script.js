ead8534d200531cd63804328914953f9f24011e2
Victtor777 <victtor.eng.software@gmail.com>
2023-11-30 01:22:11 -0300

    funcionamento refatorado

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ import Tooltip from "./modules/tooltip.js";
 import ScrollAnima from "./modules/scroll-anima.js";
 import DropdownMenu from "./modules/dropdown-menu.js";
 import MenuMobile from "./modules/menu-mobile.js";
-import initFuncionamento from "./modules/funcionamento.js";
+import Funcionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js";
 import fetchBitcoin from "./modules/fetch-bitcoin.js";
 
@@ -34,11 +34,9 @@ dropdownMenu.init();
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 menuMobile.init();
 
+const funcionamento = new Funcionamento('[data-semana]');
+funcionamento.init();
+
 
 fetchAnimais("../../animaisapi.json", ".numeros-grid");
 fetchBitcoin("https://blockchain.info/ticker", '.btc-preco');
-
-
-initFuncionamento();
-
-console.log("teste 123")
